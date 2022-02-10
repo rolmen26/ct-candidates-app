78dbc2e3359b8f7369337e221edf5da723e86398
Telmo Rafael <telmo.riofrio@funiber.org>
2022-02-10 14:42:06 -0500

    first commit

---
+++ resources/views/default.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CT Evaluation</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">        
+    </head>
+
+    <body class="antialiased">
+        @section('app')
+
+        @show
+
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.4/axios.min.js" integrity="sha512-lTLt+W7MrmDfKam+r3D2LURu0F47a3QaW5nF0c6Hl0JDZ57ruei+ovbg7BrZ+0bjVJ5YgzsAWE+RreERbpPE1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        
+        @section('vue')
+        <script>
+            new Vue({
+                el: '#app',
+                vuetify: new Vuetify(),
+                data: () => ({
+                    
+                }),
+            });
+        </script>
+        @show
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+@extends('default')
+
+@section('app')
+<div id="app">
+  <v-card>
+    Hello From Laravel and Vue
+  </v-card>           
+</div>
+@endsection
+
+@section('vue')
+    <script>
+        new Vue({
+            el: '#app',
+            vuetify: new Vuetify(),
+            data: () => ({
+
+            }),
+            mounted() {
+                
+            },
+            methods: {
+
+            }                
+        });
+    </script>
+@show
